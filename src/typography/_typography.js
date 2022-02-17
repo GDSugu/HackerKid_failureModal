b0254f8dc9830246f8ab78d35bd5a2b9c53f1deb
Sai Darshan <saidarshan27@gmail.com>
2022-02-17 20:45:49 +0530

    declare all variations of font available in design system

---
+++ src/typography/_typography.js
@@ -0,0 +1,137 @@
+const heading1 = {
+  fontFamliy: 'Averia Sans Libre',
+  fontStyle: 'normal',
+  fontWeight: bold,
+  fontSize: '48px',
+};
+
+const heading2 = {
+  fontFamily: 'Averia Sans Libre',
+  fontStyle: 'normal',
+  fontWeight: 'bold',
+  fontSize: '40px',
+}
+
+const heading3 = {
+  fontFamily: 'Averia Sans Libre',
+  fontStyle: 'normal',
+  fontWeight: 'bold',
+  fontSize: '32px',
+}
+
+const heading4 = {
+  fontFamily: 'Averia Sans Libre',
+  fontStyle: 'normal',
+  fontWeight: 'bold',
+  fontSize: '28px',
+}
+
+const heading5 = {
+  fontFamily: 'Averia Sans Libre',
+  fontStyle: 'normal',
+  fontWeight: 'bold',
+  fontSize: '24px',
+}
+
+const heading6 = {
+  fontFamily: 'Averia Sans Libre',
+  fontStyle: 'normal',
+  fontWeight: 'bold',
+  fontSize: '20px',
+}
+
+const mobileTypography = {
+  heading1,
+  heading2,
+  heading3,
+  heading4,
+  heading5,
+  heading6,
+  subtitle1: {
+    fontFamily: 'Averia Sans Libre',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: '16px',
+  },
+  subtitle2: {
+    fontFamily: 'Averia Sans Libre',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '16px',
+  },
+  body: {
+    fontFamily: 'Averia Sans Libre',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '14px',
+  },
+  bodyBold: {
+    fontFamily: 'Averia Sans Libre',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: '14px',
+  },
+  caption: {
+    fontFamily: 'Averia Sans Libre',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '12px',
+  },
+  overline: {
+    fontFamily: 'Averia Sans Libre',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '10px',
+  }
+};
+
+const webTypography = {
+  heading1,
+  heading2,
+  heading3,
+  heading4,
+  heading5,
+  heading6,
+  subtitle1: heading6,
+  subtitle2:{
+    fontFamily: 'Averia Sans Libre',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '20px',
+  },
+  bodyBold: {
+    fontFamily: 'Averia Sans Libre',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: '18px',
+  },
+  body: {
+    fontFamily: 'Averia Sans Libre',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '18px',
+  },
+  captionBold: {
+    fontFamily: 'Averia Sans Libre',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: '16px',
+  },
+  caption: {
+    fontFamily: 'Averia Sans Libre',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '16px',
+  },
+  overline: {
+    fontFamily: 'Averia Sans Libre',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '14px',
+  }
+}
+
+
+module.exports = {
+  mobileTypography, webTypography
+}
