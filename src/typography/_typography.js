2f794e8e1c7417928ce00a7468bf6983a29bed73
Sai Darshan <saidarshan27@gmail.com>
2022-02-18 14:59:01 +0530

    export font family as a constant instead of including in every object

--- src/typography/_typography.js
+++ src/typography/_typography.js
@@ -1,42 +1,36 @@
-const fontFamily = 'Averia Sans Libre';
+const fontFamily = "'Averia Sans Libre'";
 
 const heading1 = {
-  fontFamliy,
   fontStyle: 'normal',
   fontWeight: bold,
   fontSize: 48,
 };
 
 const heading2 = {
-  fontFamily,
   fontStyle: 'normal',
   fontWeight: 'bold',
   fontSize: 40,
 }
 
 const heading3 = {
-  fontFamily,
   fontStyle: 'normal',
   fontWeight: 'bold',
   fontSize: 32,
 }
 
 const heading4 = {
-  fontFamily,
   fontStyle: 'normal',
   fontWeight: 'bold',
   fontSize: 28,
 }
 
 const heading5 = {
-  fontFamily,
   fontStyle: 'normal',
   fontWeight: 'bold',
   fontSize: 24,
 }
 
 const heading6 = {
-  fontFamily,
   fontStyle: 'normal',
   fontWeight: 'bold',
   fontSize: 20,
@@ -50,37 +44,31 @@ const mobileTypography = {
   heading5,
   heading6,
   subtitle1: {
-    fontFamily,
     fontStyle: 'normal',
     fontWeight: 'bold',
     fontSize: 16,
   },
   subtitle2: {
-    fontFamily,
     fontStyle: 'normal',
     fontWeight: 'normal',
     fontSize: 16,
   },
   body: {
-    fontFamily,
     fontStyle: 'normal',
     fontWeight: 'normal',
     fontSize: 14,
   },
   bodyBold: {
-    fontFamily,
     fontStyle: 'normal',
     fontWeight: 'bold',
     fontSize: 14,
   },
   caption: {
-    fontFamily,
     fontStyle: 'normal',
     fontWeight: 'normal',
     fontSize: 12,
   },
   overline: {
-    fontFamily,
     fontStyle: 'normal',
     fontWeight: 'normal',
     fontSize: 10,
@@ -96,37 +84,31 @@ const webTypography = {
   heading6,
   subtitle1: heading6,
   subtitle2:{
-    fontFamily,
     fontStyle: 'normal',
     fontWeight: 'normal',
     fontSize: 20,
   },
   bodyBold: {
-    fontFamily,
     fontStyle: 'normal',
     fontWeight: 'bold',
     fontSize: 18,
   },
   body: {
-    fontFamily,
     fontStyle: 'normal',
     fontWeight: 'normal',
     fontSize: 18,
   },
   captionBold: {
-    fontFamily,
     fontStyle: 'normal',
     fontWeight: 'bold',
     fontSize: 16,
   },
   caption: {
-    fontFamily,
     fontStyle: 'normal',
     fontWeight: 'normal',
     fontSize: 16,
   },
   overline: {
-    fontFamily,
     fontStyle: 'normal',
     fontWeight: 'normal',
     fontSize: 14,
@@ -135,5 +117,5 @@ const webTypography = {
 
 
 module.exports = {
-  mobileTypography, webTypography
+  fontFamily,mobileTypography, webTypography
 }
