636c162fe6a4ab974d61ca17fa2da03d83c9700b
Sai Darshan <saidarshan27@gmail.com>
2022-03-02 22:01:06 +0530

    remove InputField component instead have html element as it is

--- src/web/javascripts/common/components/InputField.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const InputField = (props) => (
-  <div className ="mb-3">
-    <input type={props.type} className="form-control" id="exampleFormControlInput1" placeholder={props.placeHolder} />
-  </div>
-);
-
-export default InputField;
--- src/web/javascripts/common/pages/Signin.js
+++ src/web/javascripts/common/pages/Signin.js
@@ -1,31 +1,56 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import InputField from '../components/InputField';
 import '../../../stylesheets/signin/style.scss';
 
 const Signin = () => (
   <div className='signin-form'>
-    <div>
-      <label htmlFor="exampleFormControlInput1" className="form-label overline-bold">
-      <FormattedMessage
-          defaultMessage = "Email Address"
+    <div className='px-3 pt-2'>
+      <img src='../../../../images/signin/signin-form-svg.svg' className='signin-form-svg'></img>
+      <div className="mb-3">
+        <label htmlFor="username" className="form-label overline-bold">
+          <FormattedMessage
+              defaultMessage = "Username"
+            />
+        </label>
+        <input className='form-control' type='text' name='username' id='username' placeholder='Username'/>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="password" className="form-label overline-bold">
+        <FormattedMessage
+            defaultMessage = "Password"
+          />
+        </label>
+        <input className='form-control' type='password' name='password' id='password' placeholder='Password'/>
+      </div>
+      <p className='overline text-center'>
+        <FormattedMessage
+          defaultMessage = "Forgot Password?"
         />
-      </label>
-      <InputField type='email' placeHolder='Email Address'></InputField>
+      </p>
+      <div className='login-btn-and-google-btn'>
+        <button className='btn btn-outline-secondary sign-in-with-google-btn'>
+          <span className='overline-bold'>
+            <FormattedMessage
+              defaultMessage="Sign in with Google"
+            />
+          </span>
+        </button>
+        <button className='btn btn-primary login-btn'>
+          <span className='overline-bold'>
+            <FormattedMessage
+              defaultMessage="Login"
+            />
+          </span>
+        </button>
+      </div>
     </div>
-    <div>
-      <label htmlFor="exampleFormControlInput1" className="form-label overline-bold">
-      <FormattedMessage
-          defaultMessage = "Password"
-        />
-      </label>
-      <InputField type='password' placeHolder='Password'></InputField>
-    </div>
-    <p className='overline text-center'>
-      <FormattedMessage
-        defaultMessage = "Forgot Password"
-      />
-    </p>
+    <button className='btn btn-block create-account-btn'>
+      <span className='overline-bold'>
+          <FormattedMessage
+            defaultMessage="Create a New Account"
+          />
+        </span>
+    </button>
   </div>
 );
 
