4c60ddcedb5b384f61dc49ca22ffa61c5e776e22
Sai Darshan <saidarshan27@gmail.com>
2022-03-07 09:43:54 +0530

    add nav bar with changing the visual order of login and signwithgoogle btn

--- src/web/javascripts/common/pages/Signin.js
+++ src/web/javascripts/common/pages/Signin.js
@@ -3,55 +3,67 @@ import { FormattedMessage } from 'react-intl';
 import '../../../stylesheets/signin/style.scss';
 
 const Signin = () => (
-  <div className='signin-form'>
-    <div className='px-3 pt-2'>
-      <img src='../../../../images/signin/signin-form-svg.svg' className='signin-form-svg'></img>
-      <div className="mb-3">
-        <label htmlFor="username" className="form-label overline-bold">
-          <FormattedMessage
-              defaultMessage = "Username"
-            />
-        </label>
-        <input className='form-control' type='text' name='username' id='username' placeholder='Username'/>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="password" className="form-label overline-bold">
-        <FormattedMessage
-            defaultMessage = "Password"
-          />
-        </label>
-        <input className='form-control' type='password' name='password' id='password' placeholder='Password'/>
-      </div>
-      <p className='overline text-center'>
-        <FormattedMessage
-          defaultMessage = "Forgot Password?"
-        />
+  <>
+    <nav className='d-flex justify-content-between align-items-center'>
+      <img className='logo' src='../../../../images/signin/logo_H1.svg' />
+      <p className='help caption-bold mb-0'>
+        <FormattedMessage defaultMessage="Help"/>
       </p>
-      <div className='login-btn-and-google-btn'>
-        <button className='btn btn-outline-secondary sign-in-with-google-btn'>
-          <span className='overline-bold'>
+    </nav>
+    <div className='signin-form'>
+      <div className='px-3 pt-2'>
+        <img src='../../../../images/signin/signin-form-svg.svg' className='signin-form-svg'></img>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label overline-bold">
             <FormattedMessage
-              defaultMessage="Sign in with Google"
-            />
-          </span>
-        </button>
-        <button className='btn btn-primary login-btn'>
-          <span className='overline-bold'>
-            <FormattedMessage
-              defaultMessage="Login"
-            />
-          </span>
-        </button>
-      </div>
-    </div>
-    <button className='btn btn-block create-account-btn'>
-      <span className='overline-bold'>
+                defaultMessage = "Username"
+              />
+          </label>
+          <input className='form-control' type='text' name='username' id='username' placeholder='Username'/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label overline-bold">
           <FormattedMessage
-            defaultMessage="Create a New Account"
+              defaultMessage = "Password"
+            />
+          </label>
+          <input className='form-control' type='password' name='password' id='password' placeholder='Password'/>
+        </div>
+        <p className='overline text-center'>
+          <FormattedMessage
+            defaultMessage = "Forgot Password?"
           />
-        </span>
-    </button>
-  </div>
+        </p>
+        <div className='login-btn-and-google-btn row'>
+          <div className='col-md-6 col-12 order-2 order-md-1 mt-3 mt-md-0'>
+            <button className='btn btn-outline-secondary sign-in-with-google-btn btn-block'>
+              <span className='overline-bold'>
+                <FormattedMessage
+                  defaultMessage="Sign in with Google"
+                />
+              </span>
+            </button>
+          </div>
+          <div className='col-md-6 col-12 order-1 order-md-2'>
+            <button className='btn btn-primary login-btn btn-block'>
+              <span className='overline-bold'>
+                <FormattedMessage
+                  defaultMessage="Login"
+                />
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <button className='btn btn-block create-account-btn'>
+        <span className='overline-bold'>
+            <FormattedMessage
+              defaultMessage="Create a New Account"
+            />
+          </span>
+      </button>
+    </div>
+  </>
 );
 
 export default Signin;
