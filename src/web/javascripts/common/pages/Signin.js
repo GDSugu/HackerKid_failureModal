4830990c7faadbb74229023aeb5a62d7f04f5c4c
Sai Darshan <saidarshan27@gmail.com>
2022-03-14 15:25:26 +0530

    customize according to new design

--- src/web/javascripts/common/pages/Signin.js
+++ src/web/javascripts/common/pages/Signin.js
@@ -8,59 +8,57 @@ const Signin = () => {
 
   return (
     <>
-      <nav className='d-none d-sm-flex justify-content-between align-items-center '>
+      <nav className='d-flex justify-content-between align-items-center'>
         <img className='logo-img' src='../../../../images/signin/logo_H1.svg' />
         <p className='help caption-bold mb-0'>
           <FormattedMessage defaultMessage="Help"/>
         </p>
       </nav>
-      <div className='signin-form'>
-        <div className='p-3 w-100'>
-          <img src='../../../../images/signin/signin-form-svg.svg' className='signin-form-svg'></img>
-          <div className="mb-3">
-            <label htmlFor="username" className="form-label overline-bold">
-              <FormattedMessage
-                  defaultMessage = "Phone or Email"
-                />
-            </label>
-            <input className='form-control' type='text' name='username' id='username' placeholder='Username'/>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label overline-bold">
+      <div className='signin-form p-3'>
+        <img src='../../../../images/signin/signin-form-svg.svg' className='signin-form-svg'></img>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label overline-bold">
             <FormattedMessage
-                defaultMessage = "Password"
+                defaultMessage = "Phone or Email"
               />
-            </label>
-            <input className='form-control' type='password' name='password' id='password' placeholder='Password'/>
-          </div>
-          <p className='overline-bold text-center forgot-password'>
-            <FormattedMessage
-              defaultMessage = "Forgot Password?"
+          </label>
+          <input className='form-control' type='text' name='username' id='username' placeholder='Username'/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label overline-bold">
+          <FormattedMessage
+              defaultMessage = "Password"
             />
-          </p>
-          <div className='take-action-buttons'>
-            <button className='btn login-btn btn-block'>
-              <span className='overline-bold'>
-                <FormattedMessage
-                  defaultMessage="Login"
-                />
-              </span>
-            </button>
-            <button className='btn btn-block login-with-otp-btn'>
-              <span className='overline-bold'>
-                <FormattedMessage
-                  defaultMessage="Login with OTP"
-                />
-              </span>
-            </button>
-            <button className='btn btn-block create-new-account-btn'>
-              <span className='overline-bold'>
-                <FormattedMessage
-                  defaultMessage="Create a New Account"
-                />
-              </span>
-            </button>
-          </div>
+          </label>
+          <input className='form-control' type='password' name='password' id='password' placeholder='Password'/>
+        </div>
+        <p className='overline-bold text-center forgot-password'>
+          <FormattedMessage
+            defaultMessage = "Forgot Password?"
+          />
+        </p>
+        <div className='take-action-buttons'>
+          <button className='btn btn-primary login-btn btn-block'>
+            <span className='overline-bold'>
+              <FormattedMessage
+                defaultMessage="Login"
+              />
+            </span>
+          </button>
+          <button className='btn btn-block login-with-otp-btn'>
+            <span className='overline-bold'>
+              <FormattedMessage
+                defaultMessage="Login with OTP"
+              />
+            </span>
+          </button>
+          <button className='btn btn-block create-new-account-btn'>
+            <span className='overline-bold'>
+              <FormattedMessage
+                defaultMessage="Create a New Account"
+              />
+            </span>
+          </button>
         </div>
       </div>
     </>
