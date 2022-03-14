7826464a7b7bb38eede2b0b1514a82531aae520f
Sai Darshan <saidarshan27@gmail.com>
2022-03-14 15:25:26 +0530

    customize according to new design

--- src/web/stylesheets/common/pages/signin/_signinTheme.js
+++ src/web/stylesheets/common/pages/signin/_signinTheme.js
@@ -4,27 +4,24 @@ const {
 
 const thememaplight = {
   'signin-text-color-1': Theme.light.utilColors.dark,
-  'signin-input-border-color': Theme.light.screenYellow.inputBorderColor,
-  'signin-btn-bg': Theme.light.screenYellow.btnBg,
-  'signin-faded-btn-bg': Theme.light.screenYellow.fadedBtnBg,
-  'signin-faded-btn-text-color': Theme.light.screenYellow.fadedBtnTextColor,
+  'signin-text-color-2': Theme.light.screenYellow.fadedBtnTextColor,
+  'signin-border-color': Theme.light.screenYellow.inputBorderColor,
+  'signin-login-btn-bg': Theme.light.screenYellow.btnBg,
 };
 // just copied the light theme colors to the dark for now
   // TODO: make this a real dark theme with appropriate colors
 const thememapdark = {
   'signin-text-color-1': Theme.dark.utilColors.dark,
-  'signin-input-border-color': Theme.dark.screenYellow.inputBorderColor,
-  'signin-btn-bg': Theme.dark.screenYellow.btnBg,
-  'signin-faded-btn-bg': Theme.dark.screenYellow.fadedBtnBg,
-  'signin-faded-btn-text-color': Theme.dark.screenYellow.fadedBtnTextColor,
+  'signin-text-color-2': Theme.dark.screenYellow.fadedBtnTextColor,
+  'signin-border-color': Theme.dark.screenYellow.inputBorderColor,
+  'signin-login-btn-bg': Theme.dark.screenYellow.btnBg,
 };
 
 const cssvars = {
   'signin-text-color-1': '--signin-text-color-1',
-  'signin-input-border-color': '--signin-input-border-color',
-  'signin-btn-bg': '--signin-btn-bg',
-  'signin-faded-btn-bg': '--signin-faded-btn-bg',
-  'signin-faded-btn-text-color': '--signin-faded-btn-text-color',
+  'signin-text-color-2': '--signin-text-color-2',
+  'signin-border-color': '--signin-border-color',
+  'signin-login-btn-bg': '--signin-btn-bg',
 };
 
 module.exports = {
