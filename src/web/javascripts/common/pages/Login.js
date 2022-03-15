44f3271bbc9ba0a03525ca8a59ebcb9a774b56e6
Sai Darshan <saidarshan27@gmail.com>
2022-03-15 17:17:48 +0530

    change hoverables to a tags

--- src/web/javascripts/common/pages/Login.js
+++ src/web/javascripts/common/pages/Login.js
@@ -10,9 +10,9 @@ const Signin = () => {
     <>
       <nav className='d-flex justify-content-between align-items-center'>
         <img className='logo-img' src='../../../../images/signin/logo_H1.svg' />
-        <p className='help caption-bold mb-0'>
+        <a href="#" className='help caption-bold mb-0'>
           <FormattedMessage defaultMessage="Help"/>
-        </p>
+        </a>
       </nav>
       <div className='signin-form'>
         <div className='p-3 w-100'>
@@ -47,40 +47,34 @@ const Signin = () => {
             </div>
             <input className='form-control' type='password' name='password' id='password' placeholder='Password'/>
           </div>
-          <p className='overline-bold text-center forgot-password'>
+          <a href='#' className='overline-bold text-center forgot-password'>
             <FormattedMessage
               defaultMessage = "Forgot Password?"
             />
-          </label>
-          <input className='form-control' type='password' name='password' id='password' placeholder='Password'/>
-        </div>
-        <p className='overline-bold text-center forgot-password'>
-          <FormattedMessage
-            defaultMessage = "Forgot Password?"
-          />
-        </p>
-        <div className='take-action-buttons'>
-          <button className='btn login-btn btn-block'>
-            <span className='overline-bold'>
-              <FormattedMessage
-                defaultMessage="Login"
-              />
-            </span>
-          </button>
-          <button className='btn btn-block login-with-otp-btn'>
-            <span className='overline-bold'>
-              <FormattedMessage
-                defaultMessage="Login with OTP"
-              />
-            </span>
-          </button>
-          <button className='btn btn-block create-new-account-btn'>
-            <span className='overline-bold'>
-              <FormattedMessage
-                defaultMessage="Create a New Account"
-              />
-            </span>
-          </button>
+          </a>
+          <div className='take-action-buttons'>
+            <button className='btn login-btn btn-block'>
+              <span className='overline-bold'>
+                <FormattedMessage
+                  defaultMessage="Login"
+                />
+              </span>
+            </button>
+            <button className='btn btn-block login-with-otp-btn'>
+              <span className='overline-bold'>
+                <FormattedMessage
+                  defaultMessage="Login with OTP"
+                />
+              </span>
+            </button>
+            <button className='btn btn-block create-new-account-btn'>
+              <span className='overline-bold'>
+                <FormattedMessage
+                  defaultMessage="Create a New Account"
+                />
+              </span>
+            </button>
+          </div>
         </div>
       </div>
     </>
