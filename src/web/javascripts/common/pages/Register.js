1fffeab72d06bba5c4f3998dc3ab27327d809512
Sai Darshan <saidarshan27@gmail.com>
2022-03-16 00:36:06 +0530

    add register style.scss and registerTheme.js

--- src/web/javascripts/common/pages/Register.js
+++ src/web/javascripts/common/pages/Register.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { pageInit } from '../framework';
 import Header from '../components/loginHeader/Header';
+import '../../../stylesheets/common/pages/register/style.scss';
 
 const Register = () => {
   pageInit('register-container', 'Register');
