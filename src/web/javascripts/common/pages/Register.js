cd1c1020dd5985418567179f8cdd73d9c74b52e5
Sai Darshan <saidarshan27@gmail.com>
2022-03-17 01:01:46 +0530

    separate the fields into step one and step two, add clickHandlers for next btn and back btn

--- src/web/javascripts/common/pages/Register.js
+++ src/web/javascripts/common/pages/Register.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import $ from 'jquery';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
 import { pageInit } from '../framework';
 import '../../../stylesheets/common/pages/register/style.scss';
+
+const nextBtnClickHandler = (e) => {
+  e.preventDefault();
+  $('.step-1-fields').hide();
+  $('.step-2-fields').addClass('d-block');
+  $('.back-btn').addClass('d-block');
+};
+
+const backBtnClickHandler = (e) => {
+  e.preventDefault();
+
+  $('.step-1-fields').show();
+  $('.step-2-fields').removeClass('d-block');
+  $('.back-btn').removeClass('d-block');
+};
 
 const Register = () => {
   pageInit('auth-container', 'Register');
@@ -11,82 +27,122 @@ const Register = () => {
     <>
       <div className='form-container'>
         <form className='p-3 w-100'>
-          <header className='subtitle1 text-center'>Create a New Account</header>
+          <header className='d-flex'>
+            <span className='d-none back-btn' onClick={backBtnClickHandler}>back</span>
+            <p className='subtitle1 text-center flex-grow-1'>Create a New Account</p>
+          </header>
           <img src='../../../../images/register/register-form-svg.svg' className='form-svg' />
-          <div className="mb-3 form-group">
-            <div className='d-flex justify-content-between label-with-helper'>
-              <label htmlFor="username" className="form-label overline-bold">
-                <FormattedMessage
-                    defaultMessage = "Phone"
+          <div className='step-2-fields d-none'>
+            <div className="mb-3 form-group">
+                <div className='d-flex justify-content-between label-with-helper'>
+                  <label htmlFor="password" className="form-label overline-bold">
+                    <FormattedMessage
+                        defaultMessage = "Set a Password"
+                      />
+                  </label>
+                  <span className='form-helper danger overline-bold d-none'>
+                    <FormattedMessage
+                      defaultMessage = "Enter a valid username"
+                    />
+                  </span>
+                </div>
+                <input className='form-control' type='password' name='password' id='password' placeholder='Password' />
+            </div>
+            <div className="mb-3 form-group">
+                <div className='d-flex justify-content-between label-with-helper'>
+                  <label htmlFor="retyped-password" className="form-label overline-bold">
+                    <FormattedMessage
+                        defaultMessage = "Re-type password"
+                      />
+                  </label>
+                  <span className='form-helper danger overline-bold d-none'>
+                    <FormattedMessage
+                      defaultMessage = "Enter a valid username"
+                    />
+                  </span>
+                </div>
+                <input className='form-control' type='password' name='retyped-password' id='retyped-password' placeholder='Re-type Password' />
+            </div>
+            <div className='take-action-buttons'>
+                <button className='btn btn-primary btn-block'>Create Account</button>
+            </div>
+          </div>
+          <div className='step-1-fields'>
+            <div className="mb-3 form-group">
+              <div className='label-with-helper d-flex justify-content-between'>
+                <label htmlFor="username" className="form-label overline-bold">
+                  <FormattedMessage
+                      defaultMessage = "Phone"
+                    />
+                </label>
+                <span className='form-helper danger overline-bold d-none'>
+                  <FormattedMessage
+                    defaultMessage = "Enter a valid username"
                   />
-              </label>
-              <span className='form-helper danger overline-bold d-none'>
-                <FormattedMessage
-                  defaultMessage = "Enter a valid username"
-                />
-              </span>
+                </span>
+              </div>
+              <input className='form-control' type='text' name='username' id='username' placeholder='Phone or Email'/>
             </div>
-            <input className='form-control' type='text' name='username' id='username' placeholder='Phone or Email'/>
-          </div>
-          <div className="mb-3 form-group">
-            <div className='d-flex justify-content-between label-with-helper'>
-              <label htmlFor="email" className="form-label overline-bold">
-                <FormattedMessage
-                    defaultMessage = "Email"
-                />
-              </label>
-              <span className='form-helper danger overline-bold d-none'>
-                <FormattedMessage
-                  defaultMessage = "Password or username does not match"
-                />
-              </span>
+            <div className="mb-3 form-group">
+              <div className='label-with-helper d-flex justify-content-between'>
+                <label htmlFor="email" className="form-label overline-bold">
+                  <FormattedMessage
+                      defaultMessage = "Email"
+                  />
+                </label>
+                <span className='form-helper danger overline-bold d-none'>
+                  <FormattedMessage
+                    defaultMessage = "Password or username does not match"
+                  />
+                </span>
+              </div>
+              <input className='form-control' type='text' name='email' id='email' placeholder='Email'/>
             </div>
-            <input className='form-control' type='text' name='email' id='email' placeholder='Email'/>
-          </div>
-          <div className="mb-3 form-group">
-            <div className='d-flex justify-content-between label-with-helper'>
-              <label htmlFor="name" className="form-label overline-bold">
-                <FormattedMessage
-                    defaultMessage = "Name"
-                />
-              </label>
-              <span className='form-helper danger overline-bold d-none'>
-                <FormattedMessage
-                  defaultMessage = "Password or username does not match"
-                />
-              </span>
+            <div className="mb-3 form-group">
+              <div className='d-flex justify-content-between label-with-helper'>
+                <label htmlFor="name" className="form-label overline-bold">
+                  <FormattedMessage
+                      defaultMessage = "Name"
+                  />
+                </label>
+                <span className='form-helper danger overline-bold d-none'>
+                  <FormattedMessage
+                    defaultMessage = "Password or username does not match"
+                  />
+                </span>
+              </div>
+              <input className='form-control' type='text' name='fullname' id='name' placeholder='Name'/>
             </div>
-            <input className='form-control' type='text' name='fullname' id='name' placeholder='Name'/>
-          </div>
-          <div className="mb-3 form-group">
-            <div className='d-flex justify-content-between label-with-helper'>
-              <label htmlFor="parent-name" className="form-label overline-bold">
-                <FormattedMessage
-                    defaultMessage = "Parent's Name"
-                />
-              </label>
-              <span className='form-helper danger overline-bold d-none'>
-                <FormattedMessage
-                  defaultMessage = "Password or username does not match"
-                />
-              </span>
+            <div className="mb-3 form-group">
+              <div className='d-flex justify-content-between label-with-helper'>
+                <label htmlFor="parent-name" className="form-label overline-bold">
+                  <FormattedMessage
+                      defaultMessage = "Parent's Name"
+                  />
+                </label>
+                <span className='form-helper danger overline-bold d-none'>
+                  <FormattedMessage
+                    defaultMessage = "Password or username does not match"
+                  />
+                </span>
+              </div>
+              <input className='form-control' type='text' name='parent-name' id='parent-name' placeholder='Parent Name'/>
             </div>
-            <input className='form-control' type='text' name='parent-name' id='parent-name' placeholder='Parent Name'/>
-          </div>
-          <div className='take-action-buttons mb-3'>
-            <button className='btn btn-primary next-btn btn-block'>
-              <span className='overline-bold'>
+            <div className='take-action-buttons'>
+              <button className='btn btn-primary next-btn btn-block mb-3' onClick={nextBtnClickHandler}>
+                <span className='overline-bold'>
+                  <FormattedMessage
+                    defaultMessage="Next"
+                  />
+                </span>
+              </button>
+              <Link to='/login' className='overline-bold text-center login-into-existing-account mb-3'>
                 <FormattedMessage
-                  defaultMessage="Next"
-                />
-              </span>
-            </button>
+                    defaultMessage = "Login Into Existing Account"
+                  />
+              </Link>
+            </div>
           </div>
-          <Link to='/login' className='overline-bold text-center login-into-existing-account mb-3'>
-            <FormattedMessage
-                defaultMessage = "Login Into Existing Account"
-              />
-          </Link>
         </form>
       </div>
     </>
