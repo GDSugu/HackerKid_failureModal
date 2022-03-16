6f3b98d0bd0494b129077730834341e4a3da22ef
Sai Darshan <saidarshan27@gmail.com>
2022-03-16 23:05:57 +0530

    change a tags to Link

--- src/web/javascripts/common/pages/Login.js
+++ src/web/javascripts/common/pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
 import { pageInit } from '../framework';
 import '../../../stylesheets/common/pages/login/style.scss';
 
@@ -10,7 +11,7 @@ const Signin = () => {
     <>
       <div className='form-container'>
         <form className='p-3 w-100'>
-          <img src='../../../../images/signin/signin-form-svg.svg' className='signin-form-svg'></img>
+          <img src='../../../../images/signin/signin-form-svg.svg' className='form-svg' />
           <div className="mb-3 form-group">
             <div className='d-flex justify-content-between label-with-helper'>
               <label htmlFor="username" className="form-label overline-bold">
@@ -41,11 +42,11 @@ const Signin = () => {
             </div>
             <input className='form-control' type='password' name='password' id='password' placeholder='Password'/>
           </div>
-          <a href='#' className='overline-bold text-center forgot-password'>
+          <Link to='#' className='overline-bold text-center forgot-password'>
             <FormattedMessage
               defaultMessage = "Forgot Password?"
             />
-          </a>
+          </Link>
           <div className='take-action-buttons'>
             <button className='btn btn-primary login-btn btn-block'>
               <span className='overline-bold'>
@@ -61,13 +62,15 @@ const Signin = () => {
                 />
               </span>
             </button>
-            <button className='btn btn-outline-primary btn-block create-new-account-btn'>
-              <span className='overline-bold'>
-                <FormattedMessage
-                  defaultMessage="Create a New Account"
-                />
-              </span>
-            </button>
+            <Link to='/register' className='d-block mt-2 text-decoration-none'>
+              <button className='btn btn-outline-primary btn-block create-new-account-btn'>
+                  <span className='overline-bold'>
+                    <FormattedMessage
+                      defaultMessage="Create a New Account"
+                    />
+                  </span>
+              </button>
+            </Link>
           </div>
         </form>
       </div>
--- src/web/javascripts/common/pages/Register.js
+++ src/web/javascripts/common/pages/Register.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
 import { pageInit } from '../framework';
-import Header from '../components/loginHeader/Header';
 import '../../../stylesheets/common/pages/register/style.scss';
 
 const Register = () => {
@@ -10,7 +10,9 @@ const Register = () => {
   return (
     <>
       <div className='form-container'>
-      <form className='p-3 w-100'>
+        <form className='p-3 w-100'>
+          <header className='subtitle1 text-center'>Create a New Account</header>
+          <img src='../../../../images/register/register-form-svg.svg' className='form-svg' />
           <div className="mb-3 form-group">
             <div className='d-flex justify-content-between label-with-helper'>
               <label htmlFor="username" className="form-label overline-bold">
@@ -71,7 +73,7 @@ const Register = () => {
             </div>
             <input className='form-control' type='text' name='parent-name' id='parent-name' placeholder='Parent Name'/>
           </div>
-          <div className='take-action-buttons'>
+          <div className='take-action-buttons mb-3'>
             <button className='btn btn-primary next-btn btn-block'>
               <span className='overline-bold'>
                 <FormattedMessage
@@ -79,21 +81,12 @@ const Register = () => {
                 />
               </span>
             </button>
-            <button className='btn btn-outline-primary btn-block login-with-otp-btn'>
-              <span className='overline-bold'>
-                <FormattedMessage
-                  defaultMessage="Login with OTP"
-                />
-              </span>
-            </button>
-            <button className='btn btn-outline-primary btn-block create-new-account-btn'>
-              <span className='overline-bold'>
-                <FormattedMessage
-                  defaultMessage="Create a New Account"
-                />
-              </span>
-            </button>
           </div>
+          <Link to='/login' className='overline-bold text-center login-into-existing-account mb-3'>
+            <FormattedMessage
+                defaultMessage = "Login Into Existing Account"
+              />
+          </Link>
         </form>
       </div>
     </>
