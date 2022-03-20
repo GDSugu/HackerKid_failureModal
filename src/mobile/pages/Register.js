4740adeb39f914835f0a3757d98aaa6cf3752192
Sai Darshan <saidarshan27@gmail.com>
2022-03-21 01:02:31 +0530

    create stepper form using Tab Navigation

--- src/mobile/pages/Register.js
+++ src/mobile/pages/Register.js
@@ -9,8 +9,10 @@ import {
   ScrollView,
   KeyboardAvoidingView,
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import ThemeContext from '../components/theme';
-import SigninFormSvg from '../../images/signin/signin-form-svg.svg';
+import RegisterFormSvg from '../../images/register/register-form-svg.svg';
 
 const getStyles = (theme, utilColors, font) => StyleSheet.create({
   container: {
@@ -23,11 +25,17 @@ const getStyles = (theme, utilColors, font) => StyleSheet.create({
     marginBottom: 5,
     ...font.bodyBold,
   },
+  createAccountHeading: {
+    color: 'black',
+    marginVertical: 10,
+    ...font.heading6,
+    textAlign: 'center',
+  },
   inputField: {
     borderWidth: 1,
     borderColor: theme.inputBorderColor,
     borderRadius: 8,
-    padding: 14,
+    padding: 8,
     color: 'black',
     ...font.bodyBold,
   },
@@ -54,7 +62,7 @@ const getStyles = (theme, utilColors, font) => StyleSheet.create({
     textAlign: 'center',
     color: 'black',
   },
-  signInFormSvgContainer: {
+  registerFormSvgContainer: {
     justifycontent: 'center',
     alignItems: 'center',
     marginBottom: 50,
@@ -70,63 +78,48 @@ const getStyles = (theme, utilColors, font) => StyleSheet.create({
     marginBottom: 25,
     textAlign: 'center',
   },
+  nextBtnText: {
+    textAlign: 'left',
+  },
 });
 
-const Register = ({ navigation }) => {
-  const { font, theme } = React.useContext(ThemeContext);
-  const screenTheme = theme.screenSignin;
-  const style = getStyles(screenTheme, theme.utilColors, font);
+const Tab = createMaterialTopTabNavigator();
 
-  return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-between' }}>
+const RegisterFormStepOne = ({ style }) => (
+  <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}>
       <View style={style.container}>
-        <KeyboardAvoidingView>
-        <View style={style.signInFormSvgContainer}>
-          <SigninFormSvg/>
-        </View>
-        <View style={style.labelAndInputContainer}>
-          <Text style={style.label}>
-            <FormattedMessage
-                defaultMessage='Phone'
-                description='Phone label'
-            />
-          </Text>
-          <TextInput
-            style={style.inputField}
-              multiline={false}
-              disableFullscreenUI = {true}
-          />
-        </View>
-        <View style={style.labelAndInputContainer}>
-          <Text style={style.label}>
-            <FormattedMessage
-              defaultMessage='Email'
-              description='Email label'
-            />
-          </Text>
-          <TextInput
-            disableFullscreenUI = {true}
-            secureTextEntry={true}
-            style={style.inputField}
-            multiline={false} />
-        </View>
-        <View style={style.labelAndInputContainer}>
-          <Text style={style.label}>
-            <FormattedMessage
-              defaultMessage='Name'
-              description='Name label'
-            />
-          </Text>
-          <TextInput
-            disableFullscreenUI = {true}
-            style={style.inputField}
-            multiline={false} />
-        </View>
-        <View style={style.labelAndInputContainer}>
+      <KeyboardAvoidingView>
+      <View style={style.labelAndInputContainer}>
         <Text style={style.label}>
           <FormattedMessage
-            defaultMessage="Parent's Name"
-            description="Parent's Name label"
+              defaultMessage='Phone'
+              description='Phone label'
+          />
+        </Text>
+        <TextInput
+          style={style.inputField}
+            multiline={false}
+            disableFullscreenUI = {true}
+        />
+      </View>
+      <View style={style.labelAndInputContainer}>
+        <Text style={style.label}>
+          <FormattedMessage
+            defaultMessage='Email'
+            description='Email label'
+          />
+        </Text>
+        <TextInput
+          disableFullscreenUI = {true}
+          secureTextEntry={true}
+          style={style.inputField}
+          multiline={false} />
+      </View>
+      <View style={style.labelAndInputContainer}>
+        <Text style={style.label}>
+          <FormattedMessage
+            defaultMessage='Name'
+            description='Name label'
           />
         </Text>
         <TextInput
@@ -134,21 +127,94 @@ const Register = ({ navigation }) => {
           style={style.inputField}
           multiline={false} />
       </View>
-        </KeyboardAvoidingView>
-        <View>
-          <TouchableOpacity
-              style={style.btnPrimary}
-              title="Next">
-            <Text style={style.btnPrimaryText}>
-              <FormattedMessage defaultMessage='Next' description='Next Button'/>
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Text style={style.loginIntoExistingAccount}>Login into Existing Account</Text>
-          </TouchableOpacity>
-        </View>
+      <View style={style.labelAndInputContainer}>
+      <Text style={style.label}>
+        <FormattedMessage
+          defaultMessage="Parent's Name"
+          description="Parent's Name label"
+        />
+      </Text>
+      <TextInput
+        disableFullscreenUI = {true}
+        style={style.inputField}
+        multiline={false} />
+    </View>
+      </KeyboardAvoidingView>
+      <View>
+        <TouchableOpacity
+            style={style.btnPrimary}
+            title="Next">
+          <Text style={[style.btnPrimaryText, style.nextBtnText]}>
+            <FormattedMessage defaultMessage='Next' description='Next Button'/>
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={style.loginIntoExistingAccount}>Login into Existing Account</Text>
+        </TouchableOpacity>
       </View>
-    </ScrollView>
+    </View>
+  </ScrollView>
+);
+
+const RegisterFormStepTwo = ({ style }) => (
+    <View style={style.container}>
+    <KeyboardAvoidingView>
+    <View style={style.labelAndInputContainer}>
+      <Text style={style.label}>
+        <FormattedMessage
+            defaultMessage='Set Password'
+            description='Password Label'
+        />
+      </Text>
+      <TextInput
+        style={style.inputField}
+          multiline={false}
+          disableFullscreenUI={true}
+          secureTextEntry={true}
+      />
+    </View>
+    <View style={style.labelAndInputContainer}>
+      <Text style={style.label}>
+        <FormattedMessage
+          defaultMessage='Re-type Password'
+          description='Re-type Password label'
+        />
+      </Text>
+      <TextInput
+        disableFullscreenUI = {true}
+        secureTextEntry={true}
+        style={style.inputField}
+        multiline={false} />
+    </View>
+    </KeyboardAvoidingView>
+    <View>
+      <TouchableOpacity
+          style={style.btnPrimary}
+          title="Create Account">
+        <Text style={style.btnPrimaryText}>
+          <FormattedMessage defaultMessage='Create Account' description='Create Account Button'/>
+        </Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+);
+
+const Register = ({ navigation }) => {
+  const { font, theme } = React.useContext(ThemeContext);
+  const screenTheme = theme.screenLogin;
+  const style = getStyles(screenTheme, theme.utilColors, font);
+
+  return (
+    <NavigationContainer independent={true}>
+      <Text style={style.createAccountHeading}>Create a New Account</Text>
+      <View style={style.registerFormSvgContainer}>
+        <RegisterFormSvg/>
+      </View>
+      <Tab.Navigator>
+        <Tab.Screen options={{ tabBarStyle: { display: 'none' }, swipeEnabled: false }} name="RegisterFormStepOne" component={() => <RegisterFormStepOne style={ style }/>} />
+        <Tab.Screen options={{ tabBarStyle: { display: 'none' }, swipeEnabled: false }} name="RegisterFormStepTwo" component={() => <RegisterFormStepTwo style={ style }/>} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
