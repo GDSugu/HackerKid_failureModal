9a4b19e8a3a1092e211fe76b42e26188ee336f2f
anand abv <anandabv1206@gmail.com>
2023-02-04 19:42:33 +0530

    node fetch added for build

--- config/commonFunctions.js
+++ config/commonFunctions.js
@@ -2,6 +2,7 @@ const glob = require('glob');
 const del = require('del');
 // const axios = require('axios');
 const FormData = require('form-data');
+const fetch = require('node-fetch');
 
 const list = (pathPattern, options) => {
   return glob.sync(pathPattern, options);
@@ -16,21 +17,29 @@ const clean = (paths = [], options = {force: true, dryRun: true}) => {
   }
 };
 
-const getDynamicLinks = () => {
+const getDynamicLinks = async () => {
   const bodyFormData = new FormData();
   bodyFormData.append('myData', JSON.stringify({
     type: 'getSiteMap',
   }));
-  return fetch('https://api.hackerkid.org/v1/seo/', bodyFormData, {
+  // return fetch('https://api.hackerkid.org/v1/seo/', bodyFormData, {
+  //   headers: bodyFormData.getHeaders(),
+  // }).then((response) => {
+  //   return response.data;
+  // }).then((response) => {
+  //   if (response.status === 'success') {
+  //     return response.links;
+  //   }
+  //   return [];
+  // });
+  const response = await fetch('https://api.hackerkid.org/v1/seo/', bodyFormData, {
     headers: bodyFormData.getHeaders(),
-  }).then((response) => {
-    return response.data;
-  }).then((response) => {
-    if (response.status === 'success') {
-      return response.links;
-    }
-    return [];
   });
+  const data = await response;
+  if (data.data && data.data.status === 'success') {
+    return data.links;
+  }
+  return [];
 };
 
 module.exports = {
