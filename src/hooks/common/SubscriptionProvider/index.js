671bf4dbc7faf2b59238e8cbd98072b6628a482c
anand abv <anandabv1206@gmail.com>
2023-03-13 00:33:18 +0530

    clearsubscription on logout added

--- src/hooks/common/SubscriptionProvider/index.js
+++ src/hooks/common/SubscriptionProvider/index.js
@@ -3,12 +3,13 @@ import { SubscriptionContext, useGetSubscription } from '../../pages/root';
 
 const SubscriptionProvider = ({ children }) => {
   const isPageMounted = React.useRef(true);
-  const { subscriptionData } = useGetSubscription({ isPageMounted });
+  const { subscriptionData, clearSubscriptionData } = useGetSubscription({ isPageMounted });
   return (
     <>
       <SubscriptionContext.Provider
         value= {{
           subscriptionData,
+          clearSubscriptionData,
         }}
       >
         {children}
