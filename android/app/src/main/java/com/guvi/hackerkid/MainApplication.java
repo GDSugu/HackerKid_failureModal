66e35e2218924aaf2ad8a47fd1de5160821d2aed
anand abv <anandabv1206@gmail.com>
2023-03-16 23:20:24 +0530

    app name and package name has been changed

--- android/app/src/main/java/com/guvi/hackerkid/MainApplication.java
+++ android/app/src/main/java/com/guvi/hackerkid/MainApplication.java
@@ -1,4 +1,4 @@
-package com.hackerkid;
+package com.guvi.hackerkid;
 
 import android.app.Application;
 import android.content.Context;
@@ -73,7 +73,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.hackerkid.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.guvi.hackerkid.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- ios/hackerkidTests/hackerkidTests.m
+++ ios/hackerkidTests/hackerkidTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface hackerkidTests : XCTestCase
+@interface HackerKIDTests : XCTestCase
 
 @end
 
-@implementation hackerkidTests
+@implementation HackerKIDTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
